b011203215f7566f78f0fe7c0e45d33d8d52e6bf
rodolfoperatello <idd_rperatello@uolinc.com>
2021-04-30 15:31:04 -0300

    Creating ProductMapper

--- src/main/java/br/com/bicycleshop/bicycleshop/mapper/ProductMapper.java
+++ src/main/java/br/com/bicycleshop/bicycleshop/mapper/ProductMapper.java
@@ -1,14 +1,18 @@
 package br.com.bicycleshop.bicycleshop.mapper;
 
 import br.com.bicycleshop.bicycleshop.controller.response.ProductResponse;
+import br.com.bicycleshop.bicycleshop.entity.ProductEntity;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProductMapper {
 
     public static ProductResponse toResponse(final ProductEntity productEntity){
+        ProductResponse productResponse = new ProductResponse();
+        productResponse.setId(productEntity.getId());
+        productResponse.setName(productEntity.getName());
+        productResponse.setPrice(productEntity.getPrice());
 
+        return productResponse;
     }
-
-
 }
