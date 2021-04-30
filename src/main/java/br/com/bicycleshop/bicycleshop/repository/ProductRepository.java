dea89fa8fd89e0fc3e68859ad7dbd197f5c6b509
rodolfoperatello <idd_rperatello@uolinc.com>
2021-04-30 15:30:36 -0300

    Creating ProductRepository

--- src/main/java/br/com/bicycleshop/bicycleshop/repository/ProductRepository.java
+++ src/main/java/br/com/bicycleshop/bicycleshop/repository/ProductRepository.java
@@ -3,7 +3,10 @@ package br.com.bicycleshop.bicycleshop.repository;
 import br.com.bicycleshop.bicycleshop.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Long, ProductEntity> {
+
+    Optional<ProductEntity> findProductById(Long id);
 }
