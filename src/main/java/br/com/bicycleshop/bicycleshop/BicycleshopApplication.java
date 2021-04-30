17e7c6a25dae3d3b39861dc854db60689f3c769e
rodolfoperatello <idd_rperatello@uolinc.com>
2021-04-30 11:13:39 -0300

    Project structure

---
+++ src/main/java/br/com/bicycleshop/bicycleshop/BicycleshopApplication.java
@@ -0,0 +1,13 @@
+package br.com.bicycleshop.bicycleshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicycleshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicycleshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bicycleshop/bicycleshop/BicycleshopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bicycleshop.bicycleshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicycleshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
