9b186c7b77c1b97f8fc18cbcd3bf7c15b42d43d7
rodolfoperatello <idd_rperatello@uolinc.com>
2021-05-03 22:35:58 -0300

    Ajuste no sourceCompatibility do build.gradle

--- src/main/java/br/com/bicycleshop/bicycleshop/controller/ProductController.java
+++ src/main/java/br/com/bicycleshop/bicycleshop/controller/ProductController.java
@@ -4,6 +4,7 @@ import br.com.bicycleshop.bicycleshop.controller.response.ProductResponse;
 import br.com.bicycleshop.bicycleshop.service.ProductService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,7 @@ public class ProductController {
     }
 
     @GetMapping
-    public ResponseEntity<ProductResponse> getProductById(final Long id) {
+    public ResponseEntity<ProductResponse> getProductById(@PathVariable final Long id) {
         return ResponseEntity.ok(productService.findProductById(id));
     }
 }
--- src/test/java/br/com/bicycleshop/bicycleshop/BicycleshopApplicationTests.java
+++ src/test/java/br/com/bicycleshop/bicycleshop/BicycleshopApplicationTests.java
@@ -5,9 +5,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BicycleshopApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
