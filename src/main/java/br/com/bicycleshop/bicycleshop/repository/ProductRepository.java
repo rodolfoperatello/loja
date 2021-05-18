91a3bc08f01291a31543ddd447b96e1ffe1e519d
rodolfoperatello <rodolfoperatello@hotmail.com>
2021-05-18 07:59:48 -0300

    Ajuste nos parametros da interface JpaRepository

--- src/main/java/br/com/bicycleshop/bicycleshop/repository/ProductRepository.java
+++ src/main/java/br/com/bicycleshop/bicycleshop/repository/ProductRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Long, ProductEntity> {
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
     Optional<ProductEntity> findProductById(Long id);
 }
