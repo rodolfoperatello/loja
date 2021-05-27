7065f6c6e52f301028bb6a198f7c403c6175e8ab
rodolfoperatello <rodolfoperatello@hotmail.com>
2021-05-26 22:50:01 -0300

    Ajuste na estrutura da classe ProductController e adicão de final @RequestParam no endpoint de findAllProducts

--- src/main/java/br/com/bicycleshop/bicycleshop/controller/ProductController.java
+++ src/main/java/br/com/bicycleshop/bicycleshop/controller/ProductController.java
@@ -3,14 +3,10 @@ package br.com.bicycleshop.bicycleshop.controller;
 import br.com.bicycleshop.bicycleshop.controller.response.ProductResponse;
 import br.com.bicycleshop.bicycleshop.service.ProductService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.awt.print.Pageable;
 import java.util.List;
-
 
 @RestController
 @RequestMapping("/product")
@@ -28,7 +24,7 @@ public class ProductController {
     }
 
     @GetMapping
-    public ResponseEntity<List<ProductResponse>> findAllProducts(Pageable pageable){
+    public ResponseEntity<List<ProductResponse>> findAllProducts(@RequestParam final Pageable pageable) {
         return ResponseEntity.ok().body(productService.findAll(pageable));
     }
 }
