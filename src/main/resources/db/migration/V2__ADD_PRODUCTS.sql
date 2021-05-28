0b5fa8a913fed5fc3c7a6b1ca8716804abed075d
rodolfoperatello <rodolfoperatello@hotmail.com>
2021-05-27 22:32:54 -0300

    Criação de script para adição de produto

---
+++ src/main/resources/db/migration/V2__ADD_PRODUCTS.sql
@@ -0,0 +1,23 @@
+INSERT INTO
+CATEGORY (CATEGORY_ID, NAME)
+VALUES
+(1, 'Shirt'),
+(2, 'Cap'),
+(3, 'Shorts'),
+(4, 'Shoes');
+
+INSERT INTO
+PRODUCT (PRODUCT_ID, NAME, PRICE, CATEGORY_ID)
+VALUES
+(1, 'Nike Sportwear Icon', 100.0, 1),
+(2, 'Adidas Essentials Linear', 79.90, 1),
+(3, 'Puma Bi Logo T', 56.39, 1),
+(4, 'Nike Metal Swoosh', 79.89, 2),
+(5, 'Adidas Originals Class Trf', 89.90, 2),
+(6, 'Puma Essentials Running', 44.99, 2),
+(7, 'Nike Mesh 5.0', 79.99, 3),
+(8, 'Adidas 3-Stripes', 79.99, 3),
+(9, 'Puma Liga Core', 44.99, 3),
+(10, 'Nike Revolution 5', 229.99, 4),
+(11, 'Adidas Run Falcon', 219.99, 4),
+(12, 'Puma Flywer Runner', 179.99, 4);
